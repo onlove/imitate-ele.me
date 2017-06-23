421ac6730274f5175c9500a2544957585fd43b0f
hulinhai <hulinhai@derbysoft.com>
2017-06-23 17:21:38 +0800

    搭建骨架

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,56 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
+  <div class="app">
+    <v-header></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <a v-link="{path: '/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/seller'}">商家</a>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+  import header from './components/header/header.vue';
 
-export default {
-  components: {
-    Hello
-  }
-}
+  export default {
+    data() {
+      return {
+        seller: []
+      };
+    },
+    components: {
+      'v-header': header
+    }
+  };
 </script>
 
-<style>
-html {
-  height: 100%;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl";
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
+  .app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      border-1px(rgba(7, 17, 27, .1))
+      .tab-item
+        flex: 1
+        text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          &.active
+            color: rgb(240, 20, 20)
 
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
 
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
 
-.logo {
-  width: 100px;
-  height: 100px
-}
 </style>
