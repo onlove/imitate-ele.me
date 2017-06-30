9a7a465ebd0be9104373ae61546ea13efb6434b7
hulinhai <hulinhai@derbysoft.com>
2017-06-30 16:25:06 +0800

    add animation ball shopcart

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -26,8 +26,11 @@
                   <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span>
+                  <span class="new">￥{{food.price}}</span>
                   <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
                 </div>
               </div>
             </li>
@@ -35,13 +38,14 @@
         </li>
       </ul>
     </div>
-    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <shopcart v-ref:shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import shopcart from '../shopcart/shopcart.vue';
+  import cartcontrol from '../cartcontrol/cartcontrol.vue';
 
   const ERR_OK = 0;
   export default {
@@ -67,6 +71,17 @@
           }
         }
         return 0;
+      },
+      selectFoods() {
+        let foods = [];
+        this.goods.forEach((good) => {
+          good.foods.forEach((food) => {
+            if (food.count) {
+              foods.push(food);
+            }
+          });
+        });
+        return foods;
       }
     },
     created() {
@@ -90,6 +105,14 @@
         let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
         let el = foodList[index];
         this.foodsScroll.scrollToElement(el, 300);
+      },
+      selectFood(food, event) {
+
+      },
+      _drop(target) {
+        this.$nextTick(() => {
+          this.$refs.shopcart.drop(target);
+        });
       },
       _initScroll() {
         this.meunScroll = new BScroll(this.$els.menuWrapper, {
@@ -117,7 +140,13 @@
       }
     },
     components: {
-      shopcart
+      shopcart,
+      cartcontrol
+    },
+    events: {
+      'cart.add'(target) {
+        this._drop(target);
+      }
     }
   };
 </script>
@@ -224,6 +253,10 @@
               text-decoration: line-through
               font-size: 10px
               color:rgb(147, 153, 159)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 12px
 
 
 </style>
--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,18 +3,21 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight': totalCount > 0}">
+            <i class="icon-shopping_cart" :class="{'highlight': totalCount > 0}"></i>
           </div>
-          <div class="num"></div>
+          <div class="num" v-show="totalCount > 0">{{totalCount}}</div>
         </div>
-        <div class="price">￥{{totalPrice}}</div>
-        <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+        <div class="price" :class="{'highlight': totalPrice > 0}">￥{{totalPrice}}</div>
+        <div class="desc">另需配送费￥{{deliveryPrice}}</div>
       </div>
       <div class="content-right">
-        <div class="pay">
-          ￥{{minPrice}}元起
-        </div>
+        <div class="pay" :class="payClass">{{payDesc}}</div>
+      </div>
+    </div>
+    <div class="ball-container">
+      <div transition="drop" v-for="ball in balls" v-show="ball.show" class="ball">
+        <div class="inner inner-hook"></div>
       </div>
     </div>
   </div>
@@ -26,10 +29,12 @@
       selectFoods: {
         type: Array,
         default() {
-          return [{
-            price: 10,
-            count: 1
-          }];
+          return [
+            {
+              price: 10,
+              count: 1
+            }
+          ];
         }
       },
       deliveryPrice: {
@@ -40,6 +45,28 @@
         type: Number,
         default: 0
       }
+    },
+    data() {
+      return {
+        balls: [
+          {
+            show: false
+          },
+          {
+            show: false
+          },
+          {
+            show: false
+          },
+          {
+            show: false
+          },
+          {
+            show: false
+          }
+        ],
+        dropBalls: []
+      };
     },
     computed: {
       totalPrice() {
@@ -55,6 +82,75 @@
           count += food.count;
         });
         return count;
+      },
+      payDesc() {
+        if (this.totalPrice === 0) {
+          return `￥${this.minPrice}元起送`;
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice;
+          return `还差￥${diff}元起送`;
+        } else {
+          return '结算';
+        }
+      },
+      payClass() {
+        if (this.totalPrice < this.minPrice) {
+          return 'not-enough';
+        } else {
+          return 'enough';
+        }
+      }
+    },
+    methods: {
+      drop(el) {
+        for (let i = 0; i < this.balls.length; i++) {
+          let ball = this.balls[i];
+          if (!ball.show) {
+            ball.show = true;
+            ball.el = el;
+            this.dropBalls.push(ball);
+            return;
+          }
+        }
+      }
+    },
+    transitions: {
+      drop: {
+        beforeEnter(el) {
+          let count = this.balls.length;
+          while (count--) {
+            let ball = this.balls[count];
+            if (ball.show) {
+              let rect = ball.el.getBoundingClientRect();
+              let x = rect.left - 32;
+              let y = -(window.innerHeight - rect.top - 22);
+              el.style.display = '';
+              el.style.webkitTransform = `translate3d(${x}px,${y}px,0)`;
+              el.style.transform = `translate3d(${x}px,${y}px,0)`;
+              let inner = el.getElementsByClassName('inner-hook')[0];
+              inner.style.webkitTransform = `translate3D(${x}px,0,0)`;
+              inner.style.transform = `translate3d(${x}px,0,0)`;
+            }
+          }
+        },
+        enter(el) {
+          /* eslint-disable no-unused-vars */
+          let rf = el.offsetHeight;
+          this.$nextTick(() => {
+            el.style.webkitTransform = 'translate3d(0,0,0)';
+            el.style.transform = 'translate3d(0,0,0)';
+            let inner = el.getElementsByClassName('inner-hook')[0];
+            inner.style.webkitTransform = 'translate3d(0,0,0)';
+            inner.style.transform = 'translate3d(0,0,0)';
+          });
+        },
+        afterEnter(el) {
+          let ball = this.dropBalls.shift();
+          if (ball) {
+            ball.show = false;
+            el.style.display = 'none';
+          }
+        }
       }
     }
   };
@@ -93,10 +189,28 @@
             text-align: center
             border-radius: 50%
             background: #2b343c
+            &.highlight
+              background: rgb(0, 160, 220)
             .icon-shopping_cart
               line-height: 44px
               font-size: 24px
               color: #80858a
+              &.highlight
+                color: #fff
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            heigth: 16px
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size: 10px
+            font-weight: bold
+            color: #fff
+            background: rgb(240, 20, 20)
+            box-shadow: 0 4px 8px rgba(0, 0, 0, .4)
         .price
           display: inline-block
           vertical-align: top
@@ -107,6 +221,8 @@
           border-right: 1px solid rgba(255, 255, 255, .1)
           font-size: 16px
           font-weight: bold
+          &.highlight
+            color: #fff
         .desc
           display: inline-block
           vertical-align: top
@@ -122,5 +238,24 @@
           text-align: center
           font-size: 12px
           font-weight: bold
-          background: #2b333b
+          &.not-enough
+           background: #2b333b
+          &.enough
+           background: #00b43c;
+           color: #fff;
+    .ball-container
+      .ball
+        position: fixed
+        left: 32px
+        bottom: 22px
+        z-index: 200
+        &.drop-transition
+          transition: all 0.6s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+          .inner
+            width: 16px
+            height: 16px
+            border-radius: 50%
+            background:rgb(0, 160, 200)
+            transition: all .4s liner;
+
 </style>
